65134ac9188e1c68835ff219827014639ed0d945
geothomas <geothomas@cet.ac.in>
2020-10-10 12:49:11 +0530

    some more changes

--- flaskinto.py
+++ flaskinto.py
@@ -6,16 +6,16 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Geo Thomas',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        'date_posted': 'April 20, 2020'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Appu Mon KB',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'date_posted': 'April 27, 2020'
     }
 ]
 
